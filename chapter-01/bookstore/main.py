82c040d2e13efb8d1f67f4eb68ef43d3b393aa89
Oliver Forral <intrepion@gmail.com>
2025-11-18 05:50:33 -0800

    Defining your first API endpoint

---
+++ chapter-01/bookstore/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    return {
+        "book_id": book_id,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+    }

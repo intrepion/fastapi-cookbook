7e2777f4fa060103a28dfc29ae05d3de98e5b1e2
Oliver Forral <intrepion@gmail.com>
2025-11-18 06:06:27 -0800

    Working with path and query parameters

--- chapter-01/bookstore/main.py
+++ chapter-01/bookstore/main.py
@@ -3,6 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/authors/{author_id}")
+async def read_author(author_id: int):
+    return {"author_id": author_id, "name": "Ernest Hemingway"}
+
+
+@app.get("/books")
+async def read_books(year: int = None):
+    if year:
+        return {"year": year, "books": ["Book 1", "Book 2"]}
+    return {"books": ["All Books"]}
+
+
 @app.get("/books/{book_id}")
 async def read_book(book_id: int):
     return {

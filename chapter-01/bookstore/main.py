93962946a53c45cb9db0d9cf1ee1dbade4581a24
Oliver Forral <intrepion@gmail.com>
2025-11-18 06:31:39 -0800

    Defining and using request and response models

--- chapter-01/bookstore/main.py
+++ chapter-01/bookstore/main.py
@@ -1,11 +1,36 @@
 from fastapi import FastAPI
+from models import Book
+from pydantic import BaseModel
+
+
+class BookResponse(BaseModel):
+    title: str
+    author: str
+
 
 app = FastAPI()
+
+
+@app.get("/allbooks")
+async def read_all_books() -> list[BookResponse]:
+    return [
+        {"id": 1, "title": "1984", "author": "George Orwell"},
+        {
+            "id": 1,
+            "title": "The Great Gatsby",
+            "author": "F. Scott Fitzgerald",
+        },
+    ]
 
 
 @app.get("/authors/{author_id}")
 async def read_author(author_id: int):
     return {"author_id": author_id, "name": "Ernest Hemingway"}
+
+
+@app.post("/book")
+async def create_book(book: Book):
+    return book
 
 
 @app.get("/books")

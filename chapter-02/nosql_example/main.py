3f0a22099d8bc7bc8c24b4664e97ff7d0fb1086a
Oliver Forral <intrepion@gmail.com>
2025-11-18 19:00:58 -0800

    Integrating MongoDB for NoSQL data storage

---
+++ chapter-02/nosql_example/main.py
@@ -0,0 +1,40 @@
+from bson import ObjectId
+from database import user_collection
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class User(BaseModel):
+    name: str
+    email: str
+
+
+class UserResponse(User):
+    id: str
+
+
+@app.get("/user")
+def get_user(user_id: str):
+    db_user = user_collection.find_one(
+        {"_id": ObjectId(user_id) if ObjectId.is_valid(user_id) else None}
+    )
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_response = UserResponse(id=str(db_user["_id"]), **db_user)
+    return user_response
+
+
+@app.post("/user")
+def create_user(user: User):
+    result = user_collection.insert_one(user.model_dump(exclude_none=True))
+    user_response = UserResponse(id=str(result.inserted_id), **user.model_dump())
+    return user_response
+
+
+@app.get("/users")
+def read_users() -> list[UserResponse]:
+    return [
+        UserResponse(id=str(user["_id"]), **user) for user in user_collection.find()
+    ]

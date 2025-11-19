55328634d9d0c02a2247b672c902de08013b2526
Oliver Forral <intrepion@gmail.com>
2025-11-18 18:11:09 -0800

    Understanding CRUD operations with SQLAlchemy

--- chapter-02/sql_example/main.py
+++ chapter-02/sql_example/main.py
@@ -1,5 +1,6 @@
 from database import SessionLocal, User
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -12,7 +13,55 @@ def get_db():
         db.close()
 
 
+class UserBody(BaseModel):
+    name: str
+    email: str
+
+
 app = FastAPI()
+
+
+@app.delete("/user")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(db_user)
+    db.commit()
+    return {"detail": "User deleted"}
+
+
+@app.get("/user")
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@app.post("/user")
+def add_new_user(user: UserBody, db: Session = Depends(get_db)):
+    new_user = User(name=user.name, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.post("/user/{user_id}")
+def update_user(
+    user_id: int,
+    user: UserBody,
+    db: Session = Depends(get_db),
+):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_user.name = user.name
+    db_user.email = user.email
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 
 @app.get("/users/")
